74b87b9b1a170ce573b674df4502b12437b3e4b2
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-21 18:14:56 -0800

    moved innerHTML down

--- team_project/js/rollDice.js
+++ team_project/js/rollDice.js
@@ -8,9 +8,13 @@ function rollDice(diceAmount) {
     }
     document.getElementById("diceResult").innerHTML = result;
     var diceSum = result.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    
     console.log(diceSum);
+    result += "SUM = ${diceSum}";
 
-    return diceSum;
+    document.getElementById("diceResult").innerHTML = result;
+
+    return result;
 };
 
 /* rollDice(document.getElementById('dieCount'.value)) */
