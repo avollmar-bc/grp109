cd779b32017c313e52ec202c7d74f12e18e8cc59
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-21 18:01:20 -0800

    diceSum fix

--- team_project/js/rollDice.js
+++ team_project/js/rollDice.js
@@ -10,7 +10,7 @@ function rollDice(diceAmount) {
     var diceSum = result.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     console.log(diceSum);
 
-    result += ' SUM = ' + diceSum
+    result += "SUM = " + diceSum;
 
     return result;
 };
